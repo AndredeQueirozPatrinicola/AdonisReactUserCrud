ad251493a1d69c21dc58bc683d7d098598dbad07
André de Queiroz <andrepatrinicola@gmail.com>
2024-04-13 13:00:00 -0300

    add Login Title

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import { LoginForm } from "../components/LoginForm"
-
+import {LoginTitle} from "../components/LoginTitle"
 
 
 export const Login = () => {
 
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center mt-32">
+            <LoginTitle/>
             <LoginForm/>
         </div>
     )
